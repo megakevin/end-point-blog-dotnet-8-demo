f6ed84f0dea9ad292acde59c68e803fa35b0ebc9
Kevin Campusano <kcampusano@endpointdev.com>
2024-05-06 18:50:48 -0400

    Refactor uniqueness validation to use an abstract base class.

--- VehicleQuotes.WebApi/Validations/UniqueMakeNameAttribute.cs
+++ VehicleQuotes.WebApi/Validations/UniqueMakeNameAttribute.cs
@@ -4,29 +4,14 @@ using VehicleQuotes.WebApi.Repositories;
 
 namespace VehicleQuotes.WebApi.Validation;
 
-[AttributeUsage(AttributeTargets.Class)]
-public class UniqueMakeNameAttribute : ValidationAttribute
+public class UniqueMakeNameAttribute : BaseUniqueAttribute<Make, IMakeRepository>
 {
-    protected override ValidationResult? IsValid(object? value, ValidationContext context)
-    {
-        if (value == null) return ValidationResult.Success;
+    protected override Make? FindRecord(Make input, IMakeRepository repo) =>
+        repo.FindByName(input.Name);
 
-        var repo = context.GetService<IMakeRepository>();
-        if (repo == null) return ValidationResult.Success;
+    protected override bool IsSameRecord(Make input, Make existing) =>
+        input.ID == existing.ID;
 
-        return Validate((Make)value, repo);
-    }
-
-    private static ValidationResult? Validate(Make input, IMakeRepository repo)
-    {
-        var existing = repo.FindByName(input.Name);
-
-        if (existing != null && input.ID != existing.ID)
-        {
-            return new ValidationResult($"The Make name '{input.Name}' is already in use.");
-        }
-
-        return ValidationResult.Success;
-    }
+    protected override string GetErrorMessage(Make input) =>
+        $"The Make name '{input.Name}' is already in use.";
 }
-
