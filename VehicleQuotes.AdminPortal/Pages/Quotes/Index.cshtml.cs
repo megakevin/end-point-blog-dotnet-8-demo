2471787280dbfede395e353f1ce7f412a5a96f45
Kevin Campusano <kcampusano@endpointdev.com>
2024-06-07 13:49:38 -0400

    Add quotes index page.

---
+++ VehicleQuotes.AdminPortal/Pages/Quotes/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VehicleQuotes.WebApi;
+using VehicleQuotes.WebApi.Models;
+
+namespace VehicleQuotes.AdminPortal.Pages.Quotes;
+
+public class IndexModel : PageModel
+{
+    private readonly VehicleQuotesContext _context;
+
+    public IndexModel(VehicleQuotesContext context)
+    {
+        _context = context;
+    }
+
+    public IList<Quote> Quotes { get;set; } = default!;
+
+    public async Task OnGetAsync()
+    {
+        Quotes = await _context.Quotes
+            .Include(q => q.BodyType)
+            .Include(q => q.ModelStyleYear)
+            .Include(q => q.Size).ToListAsync();
+    }
+}
