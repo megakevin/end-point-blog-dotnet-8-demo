5aaff60c0a44173f97220ac0f287338ad6783bb6
Kevin Campusano <kcampusano@endpointdev.com>
2024-06-07 20:31:08 -0400

    Support uploading any number of quote images.

---
+++ VehicleQuotes.AdminPortal/wwwroot/js/quotes.new-images.js
@@ -0,0 +1,37 @@
+﻿window.addEventListener('DOMContentLoaded', _event => {
+    const newFileInput = document.querySelector("#new-quote-image-file");
+    newFileInput.addEventListener("change", addNewFileInput);
+
+    let imageInputCount = 0;
+
+    function addNewFileInput(event) {
+        convertToSubmittable(event.target)
+        createNewFileInput();
+
+        imageInputCount++;
+    }
+
+    function convertToSubmittable(fileInput) {
+        fileInput.setAttribute("name", "ImageFiles");
+        fileInput.setAttribute("id", `ImageFiles_${imageInputCount}`);
+
+        fileInput.removeEventListener("change", addNewFileInput);
+    }
+
+    function createNewFileInput() {
+        const template = document.querySelector("#quote-image-template");
+        const clone = template.content.cloneNode(true);
+
+        let fileInput = clone.querySelector("input");
+        fileInput.setAttribute("id", "new-quote-image-file");
+
+        appendToContainer(clone);
+
+        fileInput.addEventListener("change", addNewFileInput);
+    }
+
+    function appendToContainer(element) {
+        const container = document.querySelector("#quote-images-container");
+        container.appendChild(element);
+    }
+});
